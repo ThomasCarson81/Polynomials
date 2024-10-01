e10bd8cec89e0a16f0c19ff9efe2290966d2e71f
Thomas Carson <thomas.carson2024@gmail.com>
2024-10-01 21:07:26 +0100

    Implemented Newton's method

--- poly.py
+++ poly.py
@@ -7,3 +7,8 @@ def polynomial(x: int | float, coeffs: list[int | float]) -> float:
 def f_prime(x: int | float, coeffs):
     h = 1e-10 # use small h value for lim (h->0)
     return (polynomial(x + h, coeffs) - polynomial(x, coeffs)) / h
+def newtons_method(x, coeffs, depth):
+    if depth == 0:
+        return 1
+    prevx = newtons_method(x, coeffs, depth - 1)
+    return prevx - polynomial(prevx, coeffs) / f_prime(prevx, coeffs)
