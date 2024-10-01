162ba5953b3207e928b4be33704f2496992d8556
Thomas Carson <thomas.carson2024@gmail.com>
2024-10-01 21:07:03 +0100

    Added derivative function

--- poly.py
+++ poly.py
@@ -3,3 +3,7 @@ def polynomial(x: int | float, coeffs: list[int | float]) -> float:
     for i, coeff in enumerate(coeffs):
         ans += coeff * x**i
     return ans
+
+def f_prime(x: int | float, coeffs):
+    h = 1e-10 # use small h value for lim (h->0)
+    return (polynomial(x + h, coeffs) - polynomial(x, coeffs)) / h
